2478b37d439cf8849d7e06c9d5e5e112f000103f
ChocoParrot <lachocoparrot@gmail.com>
2020-01-29 07:53:07 +0800

    [factor] Pure and Simple

---
+++ source/setups/pure_and_simple.js
@@ -0,0 +1,27 @@
+var roles = {
+  "3": ["innocent_child", "vanilla_townie","mafioso"],
+  "4": ["innocent_child", "vanilla_townie", "vanilla_townie", "mafioso"],
+  "5": ["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "mafioso"],
+  "6": ["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "mafioso", "mafioso"],
+  "7": ["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "doctor", "consort", "mafioso"],
+  "8": ["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "doctor", "consort", "mafioso"],
+  "9" :["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "escort", "doctor", "consort", "mafioso"],
+  "10": ["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "escort", "doctor", "consort", "mafioso", "serial_killer"],
+  "11": ["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "escort", "doctor", "consort", "mafioso", "serial_killer"],
+  "12": ["innocent_child", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "sheriff", "escort", "doctor", "consort", "consigliere", "mafioso", "serial_killer"]
+};
+
+module.exports = function (players) {
+
+  var game_parameters = new Object();
+
+  var load_roles = roles[players.length.toString()];
+  return [load_roles, game_parameters];
+
+};
+
+module.exports.NAME = "Pure and Simple!";
+module.exports.VARIABLE_SETUP = false;
+module.exports.INFO = "A game with a confirmed townie at the very start.";
+module.exports.AUTHORS = ["SpikedJackson"];
+module.exports.PLAYER_LIMITS = [3, 12];
