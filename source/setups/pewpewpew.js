ade2e47ac8ee87beb53cae143b1529d49f86d504
ChocoParrot <lachocoparrot@gmail.com>
2020-02-03 10:31:35 +0800

    [refactor] Pewpewpew

--- source/setups/pewpewpew.js
+++ source/setups/pewpewpew.js
@@ -1,18 +1,18 @@
 var roles = {
-  "3": ["vigilante", "serial_killer", "vigilante"],
-  "4": ["vigilante", "vanilla_townie", "vanilla_townie", "serial_killer"],
-  "5": ["one_shot_dayshooter", "vigilante", "vanilla_townie", "vanilla_townie", "serial_killer"],
-  "6": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vigilante", "serial_killer"],
-  "7": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "one_shot_driver", "survivor", "vigilante", "serial_killer"],
-  "8": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver",  "vigilante", "serial_killer"],
-  "9": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor"],
-  "10": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor"],
-  "11": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor", "amnesiac"],
-  "12": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor", "amnesiac"],
-  "13": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor", "amnesiac"],
-  "14": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor", "amnesiac", "amnesiac"],
-  "15": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor", "amnesiac", "amnesiac", "jailkeeper"],
-  "16": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer", "doctor", "amnesiac", "amnesiac", "jailkeeper", "alien"]
+   "3": ["vigilante", "serial_killer", "vanilla_townie"],
+   "4": ["vigilante", "vanilla_townie", "vanilla_townie", "serial_killer"],
+   "5": ["vanilla_townie", "vigilante", "one_shot_driver", "vanilla_townie", "serial_killer"],
+   "6": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vigilante", "serial_killer"],
+   "7": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver", "vigilante", "serial_killer"],
+   "8": ["one_shot_dayshooter", "vanilla_townie", "vanilla_townie", "vanilla_townie", "jester", "one_shot_driver",  "vigilante", "serial_killer"],
+   "9": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "godfather", "mafioso", "vigilante", "serial_killer", "doctor"],
+   "10": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "godfather", "amnesiac", "mafioso", "vigilante", "serial_killer", "doctor"],
+   "11": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vanilla_townie", "amnesiac", "godfather", "vigilante", "serial_killer", "doctor", "mafioso"],
+   "12": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vanilla_townie", "amnesiac", "jester", "godfather", "vigilante", "serial_killer", "doctor", "mafioso"],
+   "13": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "amnesiac", "jester", "mafioso", "vigilante", "serial_killer", "doctor", "godfather"],
+   "14": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "amnesiac", "jester", "amnesiac", "vigilante", "serial_killer", "doctor", "godfather", "mafioso"],
+   "15": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "amnesiac", "joker", "amnesiac", "vigilante", "serial_killer", "doctor", "mafioso", "godfather", "jailkeeper"],
+   "16": ["one_shot_dayshooter", "one_shot_driver", "vanilla_townie", "vanilla_townie", "vanilla_townie", "vanilla_townie", "amnesiac", "joker", "amnesiac", "vigilante", "serial_killer", "doctor", "godfather", "mafioso", "jailkeeper", "alien"]
 };
 
 module.exports = function (players) {
@@ -27,5 +27,5 @@ module.exports = function (players) {
 module.exports.NAME = "Pewpewpew!";
 module.exports.VARIABLE_SETUP = false;
 module.exports.INFO = "A chaotic game of dayshooters and anarchy.";
-module.exports.AUTHORS = ["Thy-xin"];
+module.exports.AUTHORS = ["Thy-xin", "Jared"];
 module.exports.PLAYER_LIMITS = [3, 16];
