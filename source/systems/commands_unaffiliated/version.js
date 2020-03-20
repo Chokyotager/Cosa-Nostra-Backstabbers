f20671e8b07afa3a0abe18892b46eb249f8d88d6
ChocoParrot <lachocoparrot@gmail.com>
2020-03-20 11:07:51 +0800

    [refactor] version changes

--- source/systems/commands_unaffiliated/version.js
+++ source/systems/commands_unaffiliated/version.js
@@ -10,7 +10,7 @@ module.exports = async function (message, params, config) {
 
   var repository = version_info.homepage;
 
-  await message.channel.send(":sunflower: This bot is running on **" + update_name + " La Cosa Nostra " + version + "**.\nThe bot repository is located at <" + repository + ">.");
+  await message.channel.send(":sunflower: This bot is running on **" + update_name + " Cosa Nostra Backstabbers " + version + "**.\nThe bot repository is located at <" + repository + ">.");
   await message.channel.send(new Discord.Attachment(await assets["version-banner.png"], "la-cosa-nostra.png"));
 
 };
