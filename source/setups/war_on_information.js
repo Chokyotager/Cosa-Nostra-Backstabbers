32e444511351ab58e9b460dc2e955e809c85d28c
ChocoParrot <lachocoparrot@gmail.com>
2020-03-20 21:01:05 +0800

    [factor] new setup

---
+++ source/setups/war_on_information.js
@@ -0,0 +1,28 @@
+var roles = {
+  "3": ["doctor", "mafioso", "vanilla_townie"],
+  "4": ["doctor", "tracker", "mafioso", "vanilla_townie"],
+  "5": ["innocent_child", "doctor", "tracker", "mafioso", "vanilla_townie"],
+  "6": ["innocent_child", "doctor", "sheriff", "mafioso", "vanilla_townie", "social_engineer"],
+  "7": ["innocent_child", "doctor", "sheriff", "tracker", "mafioso", "vanilla_townie", "social_engineer"],
+  "8": ["innocent_child", "doctor", "sheriff", "tracker", "mafioso", "vanilla_townie", "social_engineer", "consigliere"],
+  "9": ["innocent_child", "doctor", "sheriff", "tracker", "mafioso", "vanilla_townie", "vanilla_townie", "social_engineer", "consigliere"],
+  "10": ["innocent_child", "doctor", "sheriff", "tracker", "mafioso", "vanilla_townie", "vanilla_townie", "social_engineer", "consigliere", "consort"],
+  "11": ["innocent_child", "doctor", "sheriff", "tracker", "mafioso", "vanilla_townie", "vanilla_townie", "social_engineer", "consigliere", "consort", "one_shot_vigilante"],
+  "12": ["innocent_child", "doctor", "sheriff", "tracker", "mafioso", "vanilla_townie", "vanilla_townie", "vanilla_townie", "social_engineer", "consigliere", "consort", "one_shot_vigilante"],
+  "13": ["innocent_child", "doctor", "sheriff", "tracker", "mafioso", "vanilla_townie", "vanilla_townie", "vanilla_townie", "social_engineer", "consigliere", "consort", "one_shot_vigilante", "godfather"]
+};
+
+module.exports = function (players) {
+
+  var game_parameters = new Object();
+
+  var load_roles = roles[players.length.toString()];
+  return [load_roles, game_parameters];
+
+};
+
+module.exports.NAME = "War on Information";
+module.exports.VARIABLE_SETUP = false;
+module.exports.INFO = "A game that's highly satirical on its own.";
+module.exports.AUTHORS = ["ShapeShifted"];
+module.exports.PLAYER_LIMITS = [1, 13];
